1064acc016dffcb0bf40d98833f2733243d6bd50
Gleb Pospelov <codefo@users.noreply.github.com>
2015-01-18 12:16:55 +0300

    Added handler

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
-static int s_interval = 42;
+static int s_interval = 0;
 static int s_step = 1;
 
 static void update_time(int value) {
@@ -36,6 +36,13 @@ int calculate_interval(int interval, int step) {
 void change_time_click_handler(ClickRecognizerRef recognizer, int flag) {
   s_step = calculate_step(click_number_of_clicks_counted(recognizer));
   s_interval = calculate_interval(s_interval, s_step) + flag * s_step;
+
+  if (s_interval > 59*60 + 59) {
+    s_interval = 0;
+  } else if (s_interval < 0) {
+    s_interval = 59*60 + 59;
+  }
+
   update_time(s_interval);
 }
 
@@ -47,8 +54,36 @@ void up_single_click_handler(ClickRecognizerRef recognizer, void *c) {
   change_time_click_handler(recognizer, 1);
 }
 
+void select_single_click_handler(ClickRecognizerRef recognizer, void *c) {
+  static const uint32_t const segments[] = { 500, 300, 500, 300, 500 };
+  VibePattern pat = {
+    .durations = segments,
+    .num_segments = ARRAY_LENGTH(segments),
+  };
+  vibes_enqueue_custom_pattern(pat);
+}
+
+void config_provider(Window *window) {
+  window_single_repeating_click_subscribe(
+    BUTTON_ID_DOWN, 
+    300, 
+    down_single_click_handler
+  );
+
+  window_single_repeating_click_subscribe(
+    BUTTON_ID_UP, 
+    300, 
+    up_single_click_handler
+  );
+  
+  window_single_click_subscribe(
+    BUTTON_ID_SELECT, 
+    select_single_click_handler
+  );
+}
+
 static void main_window_load(Window *window) {
-  s_time_layer = text_layer_create(GRect(4, 46, 144, 50));
+  s_time_layer = text_layer_create(GRect(0, 46, 144, 50));
 
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
@@ -58,7 +93,7 @@ static void main_window_load(Window *window) {
     fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS)
   );
 
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(
     window_get_root_layer(window), 
@@ -70,20 +105,6 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
-}
-
-void config_provider(Window *window) {
-  window_single_repeating_click_subscribe(
-    BUTTON_ID_DOWN, 
-    400, 
-    down_single_click_handler
-  );
-
-  window_single_repeating_click_subscribe(
-    BUTTON_ID_UP, 
-    400, 
-    up_single_click_handler
-  );
 }
 
 static void init() {
